0c85eee79a841a03aed793b0a979346a05fa131f
Marco Santana <marcos.maio96@gmail.com>
2023-06-26 19:31:31 -0300

    update: definition of flex on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Container } from './App.ts'
 import { Home } from './pages/Home/index.tsx'
 import { globalStyles } from './styles/global.ts'
 
 globalStyles()
 export function App() {
   return (
-    <>
+    <Container>
       <Home />
-    </>
+    </Container>
   )
 }
