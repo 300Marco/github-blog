7136a1d9ddb328eb112bbee905bd37ee900960c1
Marco Santana <marcos.maio96@gmail.com>
2023-06-26 19:30:00 -0300

    update: header background

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,15 @@
 import { styled } from '../../styles'
+import backgroundHeader from '../../assets/background-header.svg'
 
 export const HeaderContainer = styled('header', {
   backgroundColor: '$profile',
+  backgroundImage: `url(${backgroundHeader})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
   padding: '4rem 0 8.375rem',
+  width: '100%',
+  maxWidth: '2000px',
 
   display: 'flex',
   alignItems: 'center',
