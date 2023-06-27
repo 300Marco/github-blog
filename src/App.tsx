3360dc54586da28adb946d812bec030fc7f37a0b
Marco Santana <marcos.maio96@gmail.com>
2023-06-27 18:40:51 -0300

    update: installing and configuring React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Home } from './pages/Home/index.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router.tsx'
+
 import { globalStyles } from './styles/global.ts'
 
 globalStyles()
+
 export function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
